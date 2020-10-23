7bcc6f9891d7b270b920094d9ea13824c99d50ef
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2020-10-23 16:37:53 +0200

    first commit

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-cg-jan",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("testjantopic");
+                var readOneResult = consumer.Consume();
+                Console.WriteLine(readOneResult.Message.Value);
+            }
+        }    
+    }
+}
